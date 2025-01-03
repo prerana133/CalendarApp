f2612245bdee8225aa218aac18c48c31f82bb1d7
Anvesh <143001765+Anvesh1909@users.noreply.github.com>
2025-01-03 15:42:58 +0530

    changed frontend files

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 // api.js
 
-const BASE_URL = "http://localhost:5050";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5050";
 
 // Base fetch function
 const baseFetch = async (endpoint, options = {}) => {
